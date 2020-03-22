a4fe4670e9d3256428ea142a206335986c13cda3
Sebgatullah <sebgatullah.cuet.cse14@gmail.com>
2020-03-22 21:28:38 +0600

    React Component 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent'
+import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor(props){
+
+  	super(props);
+  	this.state = {
+
+  		dishes: DISHES
+  	};
+  }
+  render(){
   return (
     <div>
       <Navbar dark color="primary">
@@ -13,9 +23,12 @@ function App() {
           </div>
         </Navbar>
 
-       <Menu/> 
+       <Menu dishes= {this.state.dishes}/> 
     </div>
   );
+ 
+ }
+
 }
 
 export default App;
