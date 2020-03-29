3db3eb73c1cd1a1a979cbb3a3fbc8e45035674e4
Sebgatullah <sebgatullah.cuet.cse14@gmail.com>
2020-03-29 06:05:36 +0600

    React animation

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -11,6 +11,7 @@ import { BrowserRouter, Switch, Route, Redirect, withRouter } from 'react-router
 import {connect} from 'react-redux';
 import { postComment, fetchDishes, fetchComments, fetchPromos } from '../redux/ActionCreators';
 import { actions } from 'react-redux-form';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 const mapStateToProps = state =>{
    return{
@@ -83,15 +84,18 @@ class Main extends Component {
     <div>
   
        <Header/> 
-              
-           <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
-              <Route path= '/menu/:dishId' component={DishWithId}/>
-              <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
-             <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
-              <Redirect to="/home" />
-          </Switch>
+        <TransitionGroup>
+            <CSSTransition key={this.props.location.key} classNames="page" timeout={300}>
+              <Switch location={this.props.location}>
+                  <Route path='/home' component={HomePage} />
+                  <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />} />
+                  <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
+                  <Route path='/menu/:dishId' component={DishWithId} />
+                  <Route exact path='/contactus' component={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
+                  <Redirect to="/home" />
+              </Switch>
+            </CSSTransition>
+          </TransitionGroup>
         
 
        <Footer/> 
